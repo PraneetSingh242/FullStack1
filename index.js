8789a68248b6478fa962e497a6563deafa4de500
PraneetSingh242 <PraneetSingh242@gmail.com>
2025-03-22 02:20:21 +0530

    Day - 2 Boiler plate complete for project. Database connection established.

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors'
+import cors from 'cors';
+import db from './utils/db.js'; // sometimes we have to write .js 
+
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.BASE_URL,
     credentials: true,
     methods: ['Get', 'post', 'delete', 'options'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 const port = process.env.PORT || 4000; // 4000, 5000, 5173, 8000, 8080     
 
@@ -24,10 +30,13 @@ app.get('/praneet', (req, res) => {
   });
 
 
+
 app.get('/hitesh', (req, res) => {
     res.send('hitesh');
 })
 
+  // connect to db
+  db();
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
